05a2b6598f469518f219bc3a6b2ae2dabd90d535
Ani Paul <anipaulcv@gmail.com>
2024-08-05 22:42:00 +0800

    AUTO Update

--- Expense/Frontend.sh
+++ Expense/Frontend.sh
@@ -1,12 +1,11 @@
 dnf install nginx -y
+cp proxy.conf /etc/nginx/default.d/expense.conf
 systemctl enable nginx 
 rm -rf /usr/share/nginx/html/* 
 curl -o /tmp/frontend.zip https://expense-web-app.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
-pwd 
-ls -ltr
- cp proxy.conf /etc/nginx/default.d/expense.conf 
+
  systemctl restart nginx
 
 echo "Front is Done"
