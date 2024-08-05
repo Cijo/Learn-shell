f5c99d978156dd6dd35a795edd6e3c73a95e0e83
Ani Paul <anipaulcv@gmail.com>
2024-08-05 23:05:06 +0800

    backend.service file mentioned

--- Expense/backend.sh
+++ Expense/backend.sh
@@ -3,13 +3,14 @@ dnf module disable nodejs -y
 dnf module enable nodejs:20 -y 
 dnf install nodejs -y
 useradd expense
+cp backend.service  /etc/systemd/system/backend.service
 mkdir /app 
 curl -o /tmp/backend.zip https://expense-web-app.s3.amazonaws.com/backend.zip 
 cd /app 
 unzip /tmp/backend.zip
 cd /app 
 npm install 
-cp backend.service  /etc/systemd/system/backend.service
+
 chmod -R 775 /app
 chown -R expense:expense /app
 dnf install mysql-server -y
